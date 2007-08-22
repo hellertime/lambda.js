14cce7cd59d81f770c33befdc6d1291824d54cec
Chris Heller <heller@teragram.com>
2007-08-22 15:17:54 +0000

    checking in code here since I am going to overhaul the reducer. this is a save point

--- js/lambda.js
+++ js/lambda.js
@@ -10,6 +10,27 @@ function ()
 		return (node && this.element_p(node) && (this.var_p(node) || this.abstraction_p(node) || this.application_p(node))) ? true : false;
 	}
 
+	this.expression_to_string =
+	function (exp)
+	{
+		var x = "";
+
+		if(this.var_p(exp))
+		{
+			x = x + " " + exp.textContent;
+		}
+		else if(this.abstraction_p(exp))
+		{
+			x = x + "(L " + this.abstraction_term(exp).textContent + "." + this.expression_to_string(this.abstraction_body(exp)) + ")";
+		}
+		else if(this.application_p(exp))
+		{
+			x = x + "(" + this.expression_to_string(this.application_head(exp)) + " " + this.expression_to_string(this.application_tail(exp)) + ")";
+		}
+
+		return x;
+	}
+
 	// allows us to ignore "White Space"
 	this.element_p =
 	function (node)
@@ -79,7 +100,7 @@ function ()
 	this.expand_var =
 	function (v,environment)
 	{
-		var expansion = environment.getElementById(v.textContent);
+		var expansion = this.expand_var_p(v,environment);
 
 		if(expansion)
 		{
@@ -87,6 +108,12 @@ function ()
 		}
 
 		return v;
+	}
+
+	this.expand_var_p =
+	function (v, environment)
+	{
+		return environment.getElementById(v.textContent);
 	}
 
 	// lambda abstraction (function definition) predicate
@@ -133,6 +160,23 @@ function ()
 		return this.first_element_token(this.next_element_token(this.first_element_token(node)));
 	}
 
+	this.abstraction_eta_reduction =
+	function (abstraction)
+	{
+		var term = this.abstraction_term(abstraction),
+				body = this.abstraction_body(abstraction);
+
+		if(this.application_p(body))
+		{
+			if(!this.occurs_free(this.application_head(body),term) && this.same_var_p(term,this.application_tail(body)))
+			{
+				return this.application_head(body);
+			}
+		}
+
+		return abstraction;
+	}
+
 	// lambda application (function application) predicate
 	this.application_p =
 	function (node)
@@ -163,14 +207,14 @@ function ()
 
 	// please check that node passes application_p first
 	// (perhaps left-hand-side and right-hand-side are better names, but too long!)
-	this.application_head =
+	this.application_rator =
 	function (node)
 	{
 		return this.first_element_token(this.first_element_token(node));
 	}
 
 	// this one too
-	this.application_tail =
+	this.application_rand =
 	function (node)
 	{
 		return this.first_element_token(this.next_element_token(this.first_element_token(node)));
@@ -191,6 +235,7 @@ function ()
 	this.substitute_term_in_expression =
 	function (term,expression,to)
 	{
+		console.log("B: " + this.expression_to_string(expression) + "[" + this.expression_to_string(to) + "/" + this.expression_to_string(term) + "]");
 		var lambda = this;	
 		this.tree_map(expression, function (node)
 		{
@@ -230,13 +275,39 @@ function ()
 		}
 		else if(this.application_p(expression))
 		{
-			return (this.occurs_free(this.application_head(expression),x) || this.occurs_free(this.application_tail(expression),x)) ? true : false;
+			return (this.occurs_free(this.application_rator(expression),x) || this.occurs_free(this.application_rand(expression),x)) ? true : false;
 		}
 		else if(this.abstraction_p(expression))
 		{
 			return (!this.same_var_p(this.abstraction_term(expression),x) && this.occurs_free(this.abstraction_body(expression),x)) ? true : false;
 		}
 		return false;
+	}
+
+	// return the set of free variables in the expresion
+	this.FV =
+	function (expression)
+	{
+		var free_set = {};
+
+		if(this.var_p(expression))
+		{
+			free_set[expression.textContent] = 1;
+		}
+		else if(this.abstraction_p(expression))
+		{
+			free_set = this.FV(this.abstraction_body(expression));
+			delete free_set[this.abstraction_term(expression).textContent];		
+		}
+		else if(this.application_p(expression))
+		{
+			var fv_e1 = this.FV(this.application_rator(expression)),
+					fv_e2 = this.FV(this.application_rand(expression));
+
+			for(var v in fv_e1) { free_set[v] = 1; }
+			for(var v in fv_e2) { free_set[v] = 1; }
+		}
+		return free_set;
 	}
 
 	// test if variable x is bound in expression
@@ -256,7 +327,7 @@ function ()
 
 		if(this.application_p(expression))
 		{
-			return (this.occurs_bound(this.application_head(expression),x) || this.occurs_bound(this.application_tail(expression),x)) ? true : false;
+			return (this.occurs_bound(this.application_rator(expression),x) || this.occurs_bound(this.application_rand(expression),x)) ? true : false;
 		}
 		else if(this.abstraction_p(expression))
 		{
@@ -265,7 +336,7 @@ function ()
 		return false;
 	}
 
-	this.compute_toplevel =
+	this.compute_toplevel_x =
 	function (toplevel)
 	{
 		var inner_node = this.first_element_token(toplevel);
@@ -309,32 +380,32 @@ function ()
 		var redex = null;
 		if(this.application_p(node))
 		{
-			var head = this.application_head(node),
-					tail = this.application_tail(node);
-			if(this.abstraction_p(head))
+			var rator = this.application_rator(node),
+					rand = this.application_rand(node);
+			if(this.abstraction_p(rator))
 			{
 				redex = node;
 			}
-			else if(this.var_p(head))
+			else if(this.var_p(rator))
 			{
-				var expansion = this.expand_var(head,environment);
-				if(head !== expansion)
+				var expansion = this.expand_var(rator,environment);
+				if(rator !== expansion)
 				{
-					this.substitute_term_in_expression(head,node,expansion);
+					this.substitute_term_in_expression(rator,node,expansion);
 					redex = node;
 				}
 
-				if(this.application_p(tail) || this.abstraction_p(tail))
+				if(this.application_p(rand) || this.abstraction_p(rand))
 				{
-					redex = this.find_next_redex(tail,environment);
+					redex = this.find_next_redex(rand,environment);
 				}
 			}
 			else
 			{
-				redex = this.find_next_redex(head,environment);
+				redex = this.find_next_redex(rator,environment);
 				if(!redex)
 				{
-					redex = this.find_next_redex(tail,environment);
+					redex = this.find_next_redex(rand,environment);
 				}
 			}
 		}
@@ -397,6 +468,114 @@ function ()
 			redex = null;
 			subst_term = null;
 			return new_body;
+		}
+	}
+
+	this.reduce_application =
+	function (app, env)
+	{
+		var rator = this.application_rator(app),
+				rand = this.application_rand(app);
+
+		if(this.var_p(rator))
+		{
+			var v = null;
+			if((v = this.expand_var(rator,env)) !== rator)
+			{
+				console.log("Expanded " + this.expression_to_string(rator) + " ==> " + this.expression_to_string(v));
+				rator.parentNode.replaceChild(v.cloneNode(true),rator);
+				return true;
+			}
+			else
+			{
+				console.log("Cannot reduce application further.");
+				return false;
+			}
+		}
+		else if(this.abstraction_p(rator))
+		{
+			while(this.occurs_bound(rator,rand))
+			{
+				this.prime_rewrite_var(rand);
+			}
+			this.substitute_term_in_expression(this.abstraction_term(rator),this.abstraction_body(rator),rand);
+			var new_node = this.abstraction_body(rator);
+			app.parentNode.replaceChild(new_node,app);
+			return true;
+		}
+		else if(this.application_p(rator))
+		{
+			console.log("Think its an app: " + this.expression_to_string(rator));
+			return this.reduce_application(rator,env);
+		}
+		else
+		{
+			console.log("Called reduce-app on non-app: " + this.expression_to_string(app));
+			return false;
+		}
+	}
+
+	this.compute_toplevel =
+	function (toplevel)
+	{
+		this.reduce_x(toplevel,document);
+	}
+
+	this.redex_p =
+	function (exp, env)
+	{
+		if(this.application_p(exp))
+		{
+			var rator = this.application_rator(exp);
+
+			if(this.abstraction_p(rator))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	this.reduce_redex =
+	function (redex, env)
+	{
+		var rator = this.application_rator(redex),
+				rand = this.application_rand(redex);
+
+		// safe to eta-reduce first since eta-reduction simply lifts
+		// the inner expression returning it, or the original expression
+		rator = this.abstraction_eta_reduction(rator);
+	}
+
+	this.reduce_y =
+	function (exp, env)
+	{
+		if(this.redex_p(this.first_element_token(exp),env))
+		{
+				
+		}
+	}
+
+	this.reduce_x =
+	function (exp, env)
+	{
+		console.log("Reducing: " + this.expression_to_string(this.first_element_token(exp)));
+		if(this.application_p(this.first_element_token(exp)))
+		{
+			while(this.reduce_application(this.first_element_token(exp),env))
+			{
+				true;
+			}
+		}
+		else if(this.abstraction_p(this.fisrt_element_token(exp)))
+		{
+			console.log("Backtracking on: " + this.expression_to_string(this.first_element_token(exp)));
+			this.reduce_x(exp,env);
+			console.log("Exp after abx: " + this.expression_to_string(this.first_element_token(exp)));
+		}
+		else
+		{
+			console.log("Halted reduction on: " + this.expression_to_string(this.first_element_token(exp)));
 		}
 	}
 };
